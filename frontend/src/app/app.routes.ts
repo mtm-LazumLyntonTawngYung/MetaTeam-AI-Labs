9110726eeb0b00ced26cdd5c1625ae7e0eb67e42
mtm-LazumLyntonTawngYung <lazumlyntontawngyung@metateammyanmar.com>
2025-12-25 21:10:27 +0630

    feat: Implement structured Angular application with layout components and routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './features/pages/homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Homepage}
+]
---
+++ frontend/src/app/layout/components/header/header.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
